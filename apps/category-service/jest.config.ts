d39a568aa0f5e6ac7a8b1af56df9671c168ffac4
Musa Eleazar <musaeleazar090@gmail.com>
2024-08-27 10:24:14 +0100

    Category-service

--- apps/category-service/jest.config.ts
+++ apps/category-service/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'category-service',
+  displayName: 'auth-service',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/category-service',
+  coverageDirectory: '../../coverage/apps/auth-service',
 };
--- apps/category-service/src/app/app.module.ts
+++ apps/category-service/src/app/app.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/types/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [],
